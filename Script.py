89cd181b2429fcd1aae73cb3d8351662bb93c722
moviesenderggbot <157599470+moviesenderggbot@users.noreply.github.com>
2024-08-06 00:35:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -51,7 +51,7 @@ If You Want To Remove Api Then Copy This And Send To Bot - `/api None`"""
 
 📚 ʟɪʙʀᴀʀʏ: <a href=https://docs.pyrogram.org>𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦</a>
 
-🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=@SRD_OWNER>SRD_OWNER</a>
+🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/SRD_OWNER>SRD_OWNER</a>
 
 👥 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ: <a href=https://t.me/SRD_BOTZ> 𝐒𝐮𝐩𝐩𝐨𝐫𝐭</a>
 
